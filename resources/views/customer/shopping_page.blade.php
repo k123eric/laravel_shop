5c8995ff279f4782614a76ad45015c001fecce0e
k123eric <55881329+k123eric@users.noreply.github.com>
2020-11-27 20:10:13 +0800

    [feat]  新增商品頁面模板

---
+++ resources/views/customer/shopping_page.blade.php
@@ -0,0 +1,14 @@
+@extends('master')
+
+@section('顧客', 'Page Title')
+
+@section('sidebar')
+    @parent
+@endsection
+
+@section('content')
+    <div class="container" style="padding-top:70px">
+        <div class="row">
+        </div>
+    </div>
+@endsection
