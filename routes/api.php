4a22545723176b32dd78449b7b04ef231d9a8cbf
k123eric <55881329+k123eric@users.noreply.github.com>
2020-11-27 20:09:06 +0800

    [feat] 新增 Commodity api 路徑

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Commodity;
 use Illuminate\Http\Request;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('/commodity', 'CommodityController');
