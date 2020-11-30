377a63694628dd10dbc6dd70f7862d2166c7e4c6
k123eric <55881329+k123eric@users.noreply.github.com>
2020-11-30 08:20:10 +0800

    [feat] 新增引入 button css ， 新增至頂按鈕

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,13 +18,15 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/button.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    購物網
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -57,10 +59,13 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                                     $('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
+                                    <a class="dropdown-item" href="{{ url('/admin/commodity_management')}}"
+                                       onclick="event.preventDefault();">
+                                        {{ __('商品管理') }}
+                                    </a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
@@ -75,6 +80,25 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        <button onclick="topFunction()" id="myBtn" title="回到頂部"><img src="https://image.flaticon.com/icons/png/512/59/59043.png" style="width: 32px"></button>
+
+
+        <script>
+            window.onscroll = scrollFunction;
+
+            function scrollFunction() {
+                if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+                    document.getElementById("myBtn").style.display = "block";
+                } else {
+                    document.getElementById("myBtn").style.display = "none";
+                }
+            }
+
+            function topFunction() {
+                $("html, body").animate({scrollTop: 0}, 500); // For Chrome, Firefox, IE and Opera
+            }
+        </script>
     </div>
 </body>
 </html>
