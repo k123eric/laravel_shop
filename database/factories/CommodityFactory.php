0c24dbffe96629f9f31a805c05ee3e3b1d269779
k123eric <55881329+k123eric@users.noreply.github.com>
2020-11-30 08:12:48 +0800

    [feat] 新增介紹，圖片位置假資料

--- database/factories/CommodityFactory.php
+++ database/factories/CommodityFactory.php
@@ -8,8 +8,9 @@ use Faker\Generator as Faker;
 $factory->define(Commodity::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-//      'category' => $faker->name,
         'amount' => $faker->numberBetween($min = 0, $max = 12),
         'price' => $faker->numberBetween($min = 1000, $max = 9000),
+        'introduction' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+        'image_url' => $faker->imageUrl('640','480','cats'),
     ];
 });
