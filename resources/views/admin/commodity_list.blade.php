1ed71702e9189065a0968f6c8ae5400b9ce9a26d
k123eric <55881329+k123eric@users.noreply.github.com>
2020-11-30 08:15:55 +0800

    [feat] 新增商品展示功能

--- resources/views/admin/commodity_list.blade.php
+++ resources/views/admin/commodity_list.blade.php
@@ -7,31 +7,34 @@
 @endsection
 
 @section('content')
-    <div id=app>
-        <example-component></example-component>
-    </div>
     <div class="container" style="padding-top:70px">
         <div class="row">
             <div class="col-md-6">
-                <a href="/admin/product/new"><button class="btn btn-success">新增商品</button></a>
+                <a href="/admin/commoditys/new"><button class="btn btn-success">新增商品</button></a>
             </div>
         </div>
+
         <div class="row">
             <div class="col-md-12">
                 <table class="table table-striped">
                     <thead>
-                    <td>名稱</td>
-                    <td>價格</td>
-                    <td>描述</td>
-                    <td>操作</td>
+                        <td>名稱</td>
+                        <td align="center">價格</td>
+                        <td align="center">數量</td>
+                        <td align="center">操作</td>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>名稱</td>
-                        <td>價格</td>
-                        <td>描述</td>
-                        <td>操作</td>
-                    </tr>
+                    @foreach($commodities as $value)
+                        <tr>
+                            <td>{{$value->name}}</td>
+                            <td align="center">{{$value->price}}</td>
+                            <td align="center">{{$value->amount}}</td>
+                            <td align="center">
+                                    <button type="button" class="btn btn-primary">修改</button>
+                                    <button type="button" class="btn btn-danger">刪除</button>
+                            </td>
+                        </tr>
+                    @endforeach
                     </tbody>
                 </table>
             </div>
