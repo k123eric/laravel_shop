3a0ddbd29e7bbbcb773176f79fc547c4a355e69e
k123eric <55881329+k123eric@users.noreply.github.com>
2020-12-12 13:11:47 +0800

    feat. 增加商品新增，修改功能

--- app/Http/Controllers/CommodityController.php
+++ app/Http/Controllers/CommodityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
 use App\Http\Resources\CommodityResource;
 use App\Http\Resources\CommodityResourceCollection;
 use App\Commodity;
@@ -19,13 +20,15 @@ class CommodityController extends Controller
         return view('/admin/commodity_list')->with('commodities',Commodity::all());
     }
 
-    public function commodity_new(){
-        return view('/admin/new_commodity');
+    public function admin_update(Commodity $commodity)
+    {
+        return view('/admin/update_commodity')->with('commodity',$commodity);
     }
+
 
     public function commodity_show_all()
     {
-        return view('customer/shopping_page')->with('commodities',Commodity::all());
+        return view('/customer/shopping_page')->with('commodities',Commodity::all());
     }
 
     public  function commodity_show_one(Commodity $commodity)
@@ -33,27 +36,60 @@ class CommodityController extends Controller
         return view('/customer/commodity')->with('commodity',$commodity);
     }
 
-    public function store(Request $request)
+    public function store(Request $request,Commodity $commodity)
     {
         $request->validate([
             'name' => 'required|max:16|min:2',
             'price' => 'required',
             'amount' => 'required',
         ]);
+//
+        $commodity = new Commodity([
+            'name' => $request->name,
+            'price' => $request->price,
+            'amount' => $request->amount,
+            'introduction' => $request->introduction,
+            'image_url' => $request->image_url,
+        ]);
+//
+        $commodity -> save();
+        return redirect(url('admin/commodity'));
 
-        $commodity = Commodity::create($request->all());
-        return new CommodityResource($commodity);
+//        $commodity = Commodity::create($request->all());
+//        return new CommodityResource($commodity);
     }
 
-    public function update(Commodity $commodity, Request $request):CommodityResource
+    public function update(Request $request)
     {
-        $commodity->update($request->all());
-        return new CommodityResource($commodity);
+        $new_commodity = new Commodity([
+            'name' => $request->name,
+            'price' => $request->price,
+            'amount' => $request->amount,
+            'introduction' => $request->introduction,
+            'image_url' => $request->image_url,
+        ]);
+
+//        $new_commodity->name = $request->name;
+//        $new_commodity->price = $request->price;
+//        $new_commodity->amount = $request->amount;
+//        $new_commodity->introduction = $request->introduction;
+//        $new_commodity->image_url = $request->image_url;
+        Commodity::where('id',$request->commodity_id)->update([
+            'name' => $request->name,
+            'price' => $request->price,
+            'amount' => $request->amount,
+            'introduction' => $request->introduction,
+            'image_url' => $request->image_url,
+        ]);
+        return redirect('admin/commodity');
+
+//        $commodity->update($request->all());
+//        return new CommodityResource($commodity);
     }
 
     public function destroy(Commodity $commodity)
     {
         $commodity->delete();
-        return redirect('admin/commodity_management');
+        return redirect('admin/commodity');
     }
 }
