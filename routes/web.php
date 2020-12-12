4e1c1f81a1915dfc6ec7d254b367714e90363a7a
k123eric <55881329+k123eric@users.noreply.github.com>
2020-12-12 22:18:55 +0800

    feat. 新增註解

--- routes/web.php
+++ routes/web.php
@@ -16,36 +16,50 @@ use App\Commodity;
 Route::get('/','CommodityController@commodity_show_all')->name('shop');
 
 Route::group(['prefix'=>'admin','as'=>'admin.'], function(){
-    Route::get('/commodity', 'CommodityController@admin_show')->name('commodity_management');   //商品管理頁面
+    //商品管理頁面
+    Route::get('/commodity', 'CommodityController@admin_show')->name('commodity_management');
+    //商品新增頁面
     Route::get('/commodity/new', function (){
         return view('/admin/new_commodity');
-    })->name('new_commodity');   //商品新增頁面
+    })->name('new_commodity');
+    //商品管理頁面
     Route::get('/commodity/update/{commodity}', 'CommodityController@admin_update')->name('update_commodity');
+    //新增商品
     Route::post('/commodity/store', 'CommodityController@store')->name('store');
-    Route::post('/commodity/update', 'CommodityController@update');
-    Route::get('/commodity/delete/{commodity}','CommodityController@destroy');
+    //修改商品
+    Route::post('/commodity/update', 'CommodityController@update')->name('update');
+    //刪除商品
+//    Route::get('/commodity/delete/{commodity}','CommodityController@destroy')->name('delete');
+    Route::get('/commodity/delete/{commodity}','CommodityController@destroy')->name('delete');
+
 });
 
 Route::group(['prefix'=>'customer','as'=>'customer.'], function(){
-    Route::get('/shop', 'CommodityController@commodity_show_all')->name('shop');    //商品頁面
+    //商品頁面
+    Route::get('/shop', 'CommodityController@commodity_show_all')->name('shop');
+    //展示單一商品頁面
     Route::get('/commodity/{commodity}', 'CommodityController@commodity_show_one')->name('commodity');
+    //購物車頁面
     Route::get('/cart', 'OrderDetailController@show_cart')->name('cart');
+    //將商品存進購物車
     Route::get('/cart/add/{commodity}/{amount}', 'OrderDetailController@store');
+    //刪除購物車內商品
     Route::get('/cart/delete/{order_detail}', 'OrderDetailController@destroy');
 });
-
+//登入頁面
 Route::get('/login',function(){
     return view('/user/login');
 });
+//登入
 Route::post('/login','UserController@login')->name('login');
 
+//註冊頁面
 Route::get('/register',function(){
     return view('/user/register');
 });
+//註冊
 Route::post('/register','UserController@register')->name('register');
-
+//登出
 Route::get('/logout','UserController@logout')->name('logout');
 
 //Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
