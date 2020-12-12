983d2ba58aaed3d47a4007a76642e94590c39c83
k123eric <55881329+k123eric@users.noreply.github.com>
2020-12-12 13:13:03 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        'Collective\Html\HtmlServiceProvider',
         /*
          * Package Service Providers...
          */
@@ -228,7 +228,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
-
+        'Form' => 'Collective\Html\FormFacade',
+        'Html' => 'Collective\Html\HtmlFacade',
     ],
 
 ];
