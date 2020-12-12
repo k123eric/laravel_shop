6c90230bed865c464ec460c797520f82b2656411
k123eric <55881329+k123eric@users.noreply.github.com>
2020-12-12 14:18:52 +0800

    refactor. chage label for

--- resources/views/admin/new_commodity.blade.php
+++ resources/views/admin/new_commodity.blade.php
@@ -19,7 +19,7 @@
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-md-3 control-label" for="price">數量</label>
+                            <label class="col-md-3 control-label" for="amount">數量</label>
                             <div class="col-md-9">
                                 <input id="amount" name="amount" type="text" placeholder="商品數量" class="form-control input-md" required>
                             </div>
