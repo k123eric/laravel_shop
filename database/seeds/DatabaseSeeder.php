b033ebad7d18e20f2cf45f94901f1d8543b5a984
k123eric <55881329+k123eric@users.noreply.github.com>
2020-12-12 13:13:30 +0800

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(CommoditiesTableSeeder::class);
         $this->call(OrderDetailsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
