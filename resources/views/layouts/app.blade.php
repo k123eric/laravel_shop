57e6f92fad625d658ad8b9b19fc6709264fd0802
k123eric <55881329+k123eric@users.noreply.github.com>
2020-12-12 13:15:19 +0800

    refactor. 改變網頁呈現方式

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>@yield('title')</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,62 +19,44 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/button.css') }}" rel="stylesheet">
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    購物網
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     $('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-                                    <a class="dropdown-item" href="{{ route('admin.commodity_management')}}">
-                                        {{ __('商品管理') }}
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+        <nav class="navbar navbar-expand navbar-dark bg-dark">
+                <a class="navbar-brand" href="{{ url('/customer/shop') }}">購物網</a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{ url('/customer/shop') }}">Home</a>
+                        </li>
+                        <li class="nav-item dropdown" style="position:fixed;margin-left: 1620px">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" aria-expanded="false">
+                                @if(Auth::check())
+                                    {{Auth::user()->name}}
+                                    @else
+                                    {{__('未登入')}}
+                                @endif
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                @if(Auth::check())
+                                    <li><a class="dropdown-item" href="#">個人資料</a></li>
+                                    <li><a class="dropdown-item" href="#">購物車</a></li>
+                                    <li><a class="dropdown-item" href="#">購買紀錄</a></li>
+                                    <li><hr class="dropdown-divider"></li>
+                                    @if(Auth::user()->identity=='admin')
+                                        <li><a class="dropdown-item" href="{{route('admin.commodity_management')}}">商品管理</a></li>
+                                        <li><hr class="dropdown-divider"></li>
+                                    @endif
+                                    <li><a class="dropdown-item" href="{{route('logout')}}">登出</a></li>
+                                    @else
+                                    <li><a class="dropdown-item" href="{{ url('/login')}}">登入</a></li>
+                                    <li><a class="dropdown-item" href="{{ url('/register')}}">註冊</a></li>
+                                @endif
+                            </ul>
+                        </li>
                     </ul>
                 </div>
-            </div>
         </nav>
 
         <main class="py-4">
@@ -85,6 +67,10 @@
 
 
         <script>
+            $('document').ready(function(){
+                set_dropdown_list();
+            });
+
             window.onscroll = scrollFunction;
 
             function scrollFunction() {
@@ -103,6 +89,24 @@
                 window.location.href="{{route('customer.cart')}}";
             }
 
+            function set_dropdown_list(){
+
+                let dropdown_menu = $('.dropdown-menu');
+                let dropdown_link = $('#navbarDropdownMenuLink');
+
+                dropdown_link.on("mousedown",function(){
+                    if((dropdown_menu.css("display") === "block")) {
+                        dropdown_menu.fadeOut();
+                    }else{
+                        dropdown_menu.fadeIn();
+                    }
+                });
+
+                dropdown_menu.hover(function(){
+                },function(){
+                    dropdown_menu.fadeOut();
+                });
+            }
         </script>
     </div>
 </body>
