7a651ed0d4fa6b10ae99a8ee6422f00257981c25
k123eric <55881329+k123eric@users.noreply.github.com>
2020-12-13 23:56:33 +0800

    feat. 新增驗證機制

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 
 class UserController extends Controller
@@ -13,17 +14,25 @@ class UserController extends Controller
 
     public function register(Request $request)
     {
-        $user = new User;
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->identity = 'user';
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
         $user_exist = User::where('email',$request->email)->first();
         if(isset($user_exist)){
             return view('user/register',['message' =>'Email Already in Use!']);
         }
-        $user->save();
-        return view('/welcome',['user' => $user->name]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'identity' => 'user',
+        ]);
+
+        return redirect(route('customer.shop'))->with('status', '註冊成功!');
     }
 
     public function login(Request $request)
@@ -31,7 +40,11 @@ class UserController extends Controller
         $email = $request->email;
         $password = $request->password;
         Auth::attempt(['email'=>$email, 'password'=>$password]);
-        return redirect(route('customer.shop'));
+        if(Auth::check()){
+            return redirect(route('customer.shop'))->with('status', '登入成功!');
+        }else{
+            return redirect(route('login'))->with('status', '登入失敗!');
+        }
     }
 
     public function logout(Request $request){
@@ -41,6 +54,6 @@ class UserController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect(route('customer.shop'));
+        return redirect(route('customer.shop'))->with('status', '登出成功!');
     }
 }
