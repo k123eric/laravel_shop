fe53465ecda5cad3ff4b547e0f6cbcc73149d8c5
k123eric <55881329+k123eric@users.noreply.github.com>
2020-12-13 23:57:31 +0800

    feat. 新增操作提示

--- resources/views/customer/shopping_page.blade.php
+++ resources/views/customer/shopping_page.blade.php
@@ -3,6 +3,17 @@
 @section('title', '商店頁面')
 
 @section('content')
+    @if (session('status'))
+        <div class="status">
+            <div style="position: absolute;width:1918px;height:150%;background: #1b1e21;opacity: .3;z-index:1" >
+            </div>
+            <div class="card text-center" style=" width: 200px;height: 64px;position:fixed;top: 50%;left: 50%;margin: -32px 0 0 -100px;z-index:2">
+                <div class="card-body">
+                    <p class="card-text">{{ session('status') }}</p>
+                </div>
+            </div>
+        </div>
+    @endif
     <div class="row" style="width: 1918px; padding:0 20px 0 220px">
         @foreach($commodities as $commodity)
             <div class="card" style="width: 17rem; margin: 30px 14px 0 14px">
@@ -16,4 +27,15 @@
         @endforeach
         <button id="ShoppingCart" onclick="open_cart()"><img src="https://webstockreview.net/images/shopping-cart-icon-png-1.png" style="width: 32px"></button>
     </div>
+    <script>
+        $(document).ready(function(){
+            if($('.status') !== 'undefined'){
+                let timeoutID = window.setTimeout(( () => $('.status').remove() ), 1000);
+
+                $('.status').hover(function(){
+                    window.setTimeout(( () => $(this).remove() ), 500);
+                });
+            }
+        });
+    </script>
 @endsection
