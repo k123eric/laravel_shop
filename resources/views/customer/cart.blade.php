0f5582710ed0489492f4b4d867cce7e577610608
k123eric <55881329+k123eric@users.noreply.github.com>
2020-12-19 19:50:31 +0800

    feat. 刪除購物車內商品功能

--- resources/views/customer/cart.blade.php
+++ resources/views/customer/cart.blade.php
@@ -15,46 +15,133 @@
             </tr>
             </thead>
             <tbody>
-                @forelse ($commodities as $product)
-                    <tr class="text-center">
-                        <td style="padding-top: 20px">{{$product->name}}</td>
-                        <td style="padding-top: 20px">{{$product->price}}</td>
-                        <td style="padding-top: 20px">{{$product->buy_amount}}</td>
-                        <td style="padding-top: 20px" id="price">{{$product->price*$product->buy_amount}}</td>
-                        <td style="padding-top: 20px"><a href="/customer/cart/delete/{{$product->id}}"
-                               class="text-red-500 delete-confirm font-medium ml-2">刪除</a></td>
+            @if(isset($commodities))
+                @foreach ($commodities as $commodity)
+                    <tr class="text-center commodity" style="line-height:54px;">
+                        <td id="commodity_name" style="padding-top: 20px">
+                            {{$commodity->name}}
+                            <div class="card" id="img_card" style="max-width: 20rem;max-height: 20rem;position:absolute;z-index:999;display:none;margin-top: 20px"><img src="{{$commodity->image_url}}" class="img-fluid" alt="無法顯示圖片"></div>
+                        </td>
+                        <td style="padding-top: 20px">{{$commodity->price}}</td>
+                        <td style="padding-top: 20px">{{$commodity->buy_amount}}</td>
+                        <td style="padding-top: 20px" id="price">{{$commodity->price*$commodity->buy_amount}}</td>
+                        <td style="padding-top: 20px">
+                            <button type="button" id="plus_button" style="border:none;background-color:transparent;"><img src="https://img.icons8.com/android/24/000000/plus.png"/></button>
+                            <button type="button" id="minus_button" style="border:none;background-color:transparent;"><img src="https://img.icons8.com/android/24/000000/minus.png"/></button>
+                            <button type="button" id="delete_button" class="btn btn-danger">刪除</button>
+                        </td>
                     </tr>
-                    @empty
-                        <tr>
-                            <td class="text-center text-muted" colspan="5">
-                                現在購物車中沒有任何商品，馬上去購物吧！
-                                <a href="{{ url('/customer/shop')}}" class="btn btn-outline-primary">商品區</a>
-                            </td>
-                        </tr>
-                @endforelse
-            <tr class="text-center">
-                <td colspan="3" style="padding-top: 20px"><h5>總價</h5></td>
-                <td colspan="2" style="padding-top: 20px" id="all_price"><h5></h5></td>
-            </tr>
+                @endforeach
+                <tr class="text-center">
+                    <td colspan="3" style="padding-top: 20px"><h5>總價</h5></td>
+                    <td colspan="2" style="padding-top: 20px" id="all_price">1</td>
+                </tr>
+            @else
+                <tr>
+                    <td class="text-center text-muted" colspan="5">
+                        現在購物車中沒有任何商品，馬上去購物吧！
+                        <a href="{{ url('/customer/shop')}}" class="btn btn-outline-primary">商品區</a>
+                    </td>
+                </tr>
+            @endif
             </tbody>
         </table>
-        <a href="{{ url('/customer/shop')}}">繼續購物</a>
-        <button type="button" class="btn btn-primary" style="float:right;margin-top: 20px">前往結帳</button>
+        <div>
+            @if(isset($commodities))
+                <a href="{{ url('/customer/shop')}}">繼續購物</a>
+                <button type="button" class="btn btn-primary" style="float:right;margin-top: 10px">前往結帳</button>
+            @endif
+            @if (session('success'))
+                <div class="alert alert-success" style="margin-top: 30px">
+                    {{ session('success') }}
+                </div>
+            @endif
+            @if (session('fail'))
+                <div class="alert alert-danger" style="margin-top: 30px">
+                    {{ session('fail') }}
+                </div>
+            @endif
+        </div>
     </div>
-@endsection
 
-{{--@push('script')--}}
-<script>
-    window.onload = function(){
-        count_price();
-    }
+    <script>
+        $(document).ready(function(){
+            count_price();
+        });
 
-    function count_price(){
-        let all_price = parseInt(0,10);
-        let price = document.querySelectorAll("[id='price']");
-        for(let i = 0;i<price.length;i++){
-            all_price += parseInt(price[i].innerText,10);
+        $('.commodity').find('#commodity_name').hover(function(){
+            let img_card = $(this).parent().find('#img_card');
+            img_card.css('display','');
+        }, function(){
+            let img_card = $(this).parent().find('#img_card');
+            img_card.css('display','none');
+        });
+
+        $('#plus_button').on('click',function(){
+            console.log('plus');
+        });
+
+        $('#minus_button').on('click',function(){
+            console.log('minus');
+        });
+
+        $('.btn-danger').click(function(){
+            let commodity_name = $(this).parent().parent().find('#commodity_name')[0].innerText;
+            swal({
+                text: "確定是否刪除此商品",
+                icon: 'warning',
+                dangerMode: true,
+                buttons: {
+                    cancel: {
+                        text: "取消",
+                        value: false,
+                        visible: true,
+                        className: "",
+                        closeModal: true
+                    },
+                    confirm: {
+                        text: "確認刪除",
+                        value: true,
+                        visible: true,
+                        className: "delete_commodity",
+                        closeModal: true
+                    }
+                },
+            }).then((value)=>{
+                if(value){
+                    $.ajax(
+                        {
+                            url: '{{route('customer.cart_delete')}}',
+                            type: 'get',
+                            data:{
+                                'commodity_name': commodity_name,
+                            },
+                            dataType: "JSON",
+                            success: function (data) {
+                                swal(data.msg);
+                                let ul_commodity_name = $('#commodity_name');
+                                let ul_all_price = $('#all_price');
+                                let price = parseInt(ul_commodity_name.parent().find('#price').text(),10);
+                                let all_price = ul_all_price.text();
+                                all_price -= price;
+                                ul_all_price.text(all_price);
+                                ul_commodity_name.parent().remove();
+                            },
+                            error: function () {
+                                swal("因未知原因刪除失敗!")
+                            }
+                        });
+                }
+            });
+        });
+
+        function count_price(){
+            let all_price = parseInt(0,10);
+            let price = document.querySelectorAll("[id='price']");
+            for(let i = 0;i<price.length;i++){
+                all_price += parseInt(price[i].innerText,10);
+            }
+            $('#all_price').text(all_price);
         }
-        document.getElementById('all_price').innerText = all_price;
-    }
-</script>
+    </script>
+@endsection
