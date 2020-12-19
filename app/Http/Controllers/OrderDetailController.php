62ac0eeb1adc542d30ee73e652fdb97a5074aed6
k123eric <55881329+k123eric@users.noreply.github.com>
2020-12-19 11:53:53 +0800

    feat. 購物車功能，展示，新增，刪除

--- app/Http/Controllers/OrderDetailController.php
+++ app/Http/Controllers/OrderDetailController.php
@@ -3,30 +3,69 @@
 namespace App\Http\Controllers;
 
 use App\Commodity;
-use Illuminate\Http\Request;
 use App\OrderDetail;
+use Illuminate\Http\Request;
+use Session;
 
 
 class OrderDetailController extends Controller
 {
     public function show_cart(){
-        return view('customer/cart')->with('commodities',OrderDetail::all());
+        if (!Session::has('order_detail')) {
+            return view('customer/cart');
+        }
+        $old_order_detail = Session::get('order_detail');
+        return view('customer/cart')->with('commodities',$old_order_detail);
     }
 
     public function store(Request $request,$commodity_id,$amount)
     {
         $commodity = Commodity::where('id',$commodity_id)->first();
-        $order = new OrderDetail;
-        $order->name = $commodity->name;
-        $order->price = $commodity->price;
-        $order->buy_amount = $amount;
-        $order->save();
-        return redirect('customer/commodity/'.$commodity_id);
+
+        $old_order_detail = Session::has('order_detail') ? Session::get('order_detail') : null;
+        $order_detail = new OrderDetail;
+        $order_detail->name = $commodity-> name ;
+        $order_detail->price = $commodity->price;
+        $order_detail->buy_amount = $amount;
+        $order_detail->save();
+
+        if(!empty($old_order_detail)){
+            foreach ($old_order_detail as $buy_commodity){
+                if($order_detail->name == $buy_commodity->name){
+                    if($buy_commodity->buy_amount + $amount > $commodity->amount){
+                        return redirect('customer/commodity/'.$commodity_id)->with('fail','購買數量大於商品數量!');
+                    }
+                    $buy_commodity->buy_amount += $amount;
+                    $request->session()->put('order_detail', $old_order_detail);
+                    return redirect('customer/commodity/'.$commodity_id)->with('success','成功添加至購物車!');
+                }
+            }
+        }
+
+        $request->session()->put('order_detail', $old_order_detail);
+        $request->session()->push('order_detail', $order_detail);
+        return redirect('customer/commodity/'.$commodity_id)->with('success','成功添加至購物車!');
     }
 
-    public function destroy(OrderDetail $order_detail)
+    function destroy($commodity_id)
     {
-        $order_detail->delete();
-        return redirect('customer/cart');
+        $count_item = 0;
+        $old_order_detail = Session::has('order_detail') ? Session::get('order_detail') : null;
+
+        foreach ($old_order_detail as $commodity){
+            if($commodity_id == $commodity->id){
+                if (count($old_order_detail) == 1) {
+                    Session::forget('order_detail');
+                    return redirect('customer/cart')->with('success','刪除成功!');
+                }
+                unset($old_order_detail[$count_item]);
+                session()->put('order_detail', $old_order_detail);
+                return redirect('customer/cart')->with('success','刪除成功!');
+
+            }
+            $count_item ++;
+        }
+
+        return redirect('customer/cart')->with('fail','未知原因導致刪除失敗!');
     }
 }
